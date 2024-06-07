25635151cfa0acd8f97ee9c0e2b3c059b6b0e0de
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-07 17:02:14 +0300

    making the car

---
+++ car.js
@@ -0,0 +1,13 @@
+import * as Controls from 'Controls'
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+    }
+    draw(ctx){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
