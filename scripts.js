c058af624f48199dc331416fb71175fcef7dc2bf
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-07 20:09:29 +0300

    a "camera" is placed ontop of the car

--- scripts.js
+++ scripts.js
@@ -5,14 +5,18 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9, 4);
 const car = new Car(road.getLaneCenter(1.5), 100, 30, 50)
 
-function animate() {
-  canvas.height = window.innerHeight;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  road.draw(ctx);
-  car.update();
-  car.draw(ctx);
-
-  requestAnimationFrame(animate);
-}
-
 animate();
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate);
+}
