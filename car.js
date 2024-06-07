2830c4f03810bfed1398d33edbb0a9fc164a7a51
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-07 17:15:23 +0300

    the car has appeared

--- car.js
+++ car.js
@@ -1,13 +1,15 @@
-import * as Controls from 'Controls'
-class Car{
-    constructor(x,y,width,height){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+// Car class definition
+class Car {
+    constructor(x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
     }
-    draw(ctx){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+  
+    draw(ctx) {
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-}
+  }
+  
