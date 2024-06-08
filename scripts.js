3d8c6b8f6be8aba84a868a28880fdf57b74bb3e5
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-08 15:58:30 +0300

    making the network canvas

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-const canvas = document.getElementById('myCanvas');
-canvas.width = 200;
+const carCanvas = document.getElementById('carCanvas');
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9, 4);
-const car = new Car(road.getLaneCenter(1.5), 100, 30, 50,"KEYS")
+const networkCanvas = document.getElementById('networkCanvas');
+networkCanvas.width = 300;
+
+const carctx = carCanvas.getContext("2d");
+const networkctx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 4);
+const carCanvas = new Car(road.getLaneCenter(1.5), 100, 30, 50,"AI")
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50,"DUMMY",2),
@@ -15,18 +20,18 @@ function animate() {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders,[]);
     }
-    car.update(road.borders,traffic);
-    canvas.height = window.innerHeight;
+    carCanvas.update(road.borders,traffic);
+    carCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
+    carctx.save();
+    carctx.translate(0, -carCanvas.y + carCanvas.height * 0.7);
 
-    road.draw(ctx);
+    road.draw(carctx);
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx);
+        traffic[i].draw(carctx);
     }
-    car.draw(ctx);
+    carCanvas.draw(carctx);
 
-    ctx.restore();
+    carctx.restore();
     requestAnimationFrame(animate);
 }
