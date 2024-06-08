f9a5ea2fb3db91369cdabd978c55ebad6bb0af16
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-06-08 16:04:24 +0300

    the car is moving byitself

--- scripts.js
+++ scripts.js
@@ -1,37 +1,40 @@
-const carCanvas = document.getElementById('carCanvas');
-carCanvas.width = 200;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-const networkCanvas = document.getElementById('networkCanvas');
-networkCanvas.width = 300;
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 
-const carctx = carCanvas.getContext("2d");
-const networkctx = networkCanvas.getContext("2d");
-
-const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 4);
-const carCanvas = new Car(road.getLaneCenter(1.5), 100, 30, 50,"AI")
-
-const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50,"DUMMY",2),
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
 ];
 
 animate();
 
-function animate() {
-    for (let i = 0; i < traffic.length; i++) {
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-    carCanvas.update(road.borders,traffic);
-    carCanvas.height = window.innerHeight;
+    car.update(road.borders,traffic);
 
-    carctx.save();
-    carctx.translate(0, -carCanvas.y + carCanvas.height * 0.7);
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
 
-    road.draw(carctx);
-    for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(carctx);
+    carCtx.save();
+    carCtx.translate(0,-car.y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
     }
-    carCanvas.draw(carctx);
+    car.draw(carCtx,"blue");
 
-    carctx.restore();
+    carCtx.restore();
+
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
